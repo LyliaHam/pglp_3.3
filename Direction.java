9f60f94b3dabdcbd750b4bdd9738125686945198
Utilisateur <user@debian.arcahe.ovh>
2020-05-23 19:07:22 +0200

    Classe Direction

---
+++ Direction.java
@@ -0,0 +1,19 @@
+public class Direction {
+private String dir; 
+
+Direction(String direction) {
+	this.dir = direction;
+}
+	void EAST() {
+	}
+
+	void SOUTH() {
+	}
+	
+	void WEST() {
+	}
+	
+	void NORTH() {
+	}
+	
+}
