536d72d175fd40f57abb91a2af3bfeefc871d4c1
Utilisateur <user@debian.arcahe.ovh>
2020-05-23 19:27:24 +0200

    Test non final

---
+++ src/test/AppTest.java
@@ -0,0 +1,13 @@
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest 
+    extends TestCase
+{
+    public AppTest( String name )
+    {
+        super( name );
+    }
+
+ 
